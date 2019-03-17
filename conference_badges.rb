9102005893b53f4fe2a3074ba4c2d52e8041db53
Denali Balser <denali.balser167@gmail.com>
2019-03-17 18:59:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,3 +17,12 @@ def assign_rooms(attendees)
   end
   return rooms
 end 
+
+def assign_rooms(speakers)
+  assign=[]
+  
+  speakers.each_with_index{|speaker,index|
+  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+   }
+   return assign
+end 
